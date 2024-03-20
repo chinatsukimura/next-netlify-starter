2646390ccaf389a78ef9a09a1b976d00dec3eae2
木村 千夏 <cnt.d8sy@gmail.com>
2024-03-20 12:46:44 +0900

    Linkタグに変更

--- components/Add_button.js
+++ components/Add_button.js
@@ -1,15 +1,16 @@
 
 import React from 'react';
 import { LinkCrewH3 } from 'common';
+import Link from 'next/link';
 
 export default function Add_button() {
   return (
-    <a href="./contact">
       <div className="card bg-primary hover:cursor-pointer mx-auto w-1/5">
+        <Link href="./contact">
         <div className="card-body text-center p-4 md:p-6 lg:p-8 xl:p-10">
-          <LinkCrewH3 title="contact" className="card-title mx-auto" contact></LinkCrewH3>
+          <LinkCrewH3 title="contact" className="card-title mx-auto" />
         </div>
+        </Link>
       </div>
-    </a>
-  )
+  );
 }
